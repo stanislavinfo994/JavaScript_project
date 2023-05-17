677a4cb2a14ad4f334345d8f49fe2ebfe39b9d35
stanislavinfo994 <stanislavinfo994@gmail.com>
2023-05-17 23:40:21 +0300

    Added changes to Homework - 28

--- homeWork 28/index.js
+++ homeWork 28/index.js
@@ -1,7 +1,20 @@
 let votes = [0, 0, 0, 0, 0];
+
 function vote(index) {
     votes[index]++;
+    updateVoteCount(index);
 }
+
+// Функція для оновлення кількості голосів на сторінці
+function updateVoteCount(index) {
+    let voteCountElement = document.getElementById("votes" + index);
+    voteCountElement.textContent = String(votes[index]);
+    if (votes[index] > 0) {
+        voteCountElement.classList.add("show");
+    }
+}
+
+
 function showResults() {
     let maxVotes = Math.max(...votes);
     let winningSmiley = votes.indexOf(maxVotes);
@@ -12,6 +25,7 @@ function showResults() {
     resultContainer.innerHTML += "<div class='smiley'>" + getSmiley(winningSmiley) + "</div>";
     resultContainer.innerHTML += "<p>Кількість голосів: " + maxVotes + "</p>";
 }
+
 function getSmiley(index) {
     let smileys = ["😀", "😊", "😎", "🤩", "😍"];
     return smileys[index];
